bc10079ae0640ad3b640f52e61dddca261730a5c
Wenkai186 <wkblue186@gmail.com>
2016-08-24 10:54:13 +0200

    add the whole project

---
+++ MousePosition/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MousePosition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x = Cursor.Position.X;
+            int y = Cursor.Position.Y;
+            label1.Text = x.ToString();
+            label2.Text = y.ToString();
+        }
+    }
+}
